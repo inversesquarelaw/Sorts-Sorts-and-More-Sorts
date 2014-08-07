26a7baab687692912915c29139354aa70924d942
sadjava <r.peterson104@gmail.com>
2014-08-07 11:58:20 -0700

    Quick sort implemented

---
+++ src/com/peterson/sorts/QuickSort.java
@@ -0,0 +1,56 @@
+package com.peterson.sorts;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author Peterson, Ryan
+ *         Created 8/7/2014
+ */
+public class QuickSort
+{
+    public static <T> void sort(List<T> list, Comparator<T> comp)
+    {
+        Collections.shuffle(list);
+        _sort(list, comp, 0, list.size() - 1);
+
+    }
+
+    private static <T> void _sort(List<T> list, Comparator<T> comp, int lo, int hi)
+    {
+        if (lo < hi)
+        {
+            int partition = partition(list, comp, lo, hi);
+            _sort(list, comp, lo, partition - 1);
+            _sort(list, comp, partition + 1, hi);
+        }
+    }
+
+    private static <T> int partition(List<T> list, Comparator<T> comp, int lo, int hi)
+    {
+        int i = lo;
+        int j = hi + 1;
+        T temp = list.get(i);
+
+        //loop will end
+        while (true)
+        {
+            while (comp.compare(list.get(++i), temp) < 0)
+            {
+                ; //intentional empty loop
+            }
+            while (comp.compare(temp, list.get(--j)) > 1)
+            {
+                ; //intentional empty loop
+            }
+            if (i >= j)
+                break;
+            Collections.swap(list, i, j);
+        }
+
+        Collections.swap(list, lo, j);
+        return j;
+    }
+
+}
--- src/com/peterson/sorts/Test.java
+++ src/com/peterson/sorts/Test.java
@@ -17,13 +17,25 @@ public class Test
         }
     }
 
+    public static void isSorted(List<Integer> list)
+    {
+        Comparator<Integer> c = ComparatorUtils.integerComparator();
+        int last = 0;
+        for (int i = 1; i < list.size(); i++)
+        {
+            if (c.compare(list.get(last), list.get(i)) > 0)
+                throw new RuntimeException("List not sorted");
+            last = i;
+        }
+    }
+
     public static void main(String[] args)
     {
         List<Integer> list = new ArrayList<>();
-        randomList(list, 10);
+        randomList(list, 1000);
         InsertionSort.sort(list, ComparatorUtils.integerComparator());
 
-
+        isSorted(list);
         System.out.println(Arrays.toString(list.toArray()));
 
     }
