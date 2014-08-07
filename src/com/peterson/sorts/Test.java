da44678d9f3c820bc87f32636c193a727501e899
sadjava <r.peterson104@gmail.com>
2014-08-07 13:24:00 -0700

    Implemented tests for the sorts, with each sort doing its thing on a thread.

--- src/com/peterson/sorts/Test.java
+++ src/com/peterson/sorts/Test.java
@@ -1,5 +1,6 @@
 package com.peterson.sorts;
 
+import java.time.Clock;
 import java.util.*;
 
 /**
@@ -8,6 +9,8 @@ import java.util.*;
  */
 public class Test
 {
+    public static final int NUM_ELEMENTS = 10000;
+
     public static void randomList(List<Integer> list, int size)
     {
         Random rand = new Random();
@@ -29,13 +32,116 @@ public class Test
         }
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
-        List<Integer> list = new ArrayList<>();
-        randomList(list, 100);
+        for (int j = 0; j < 10; j++)
+        {
+            SortThread[] sThreads = new SortThread[4];
+            sThreads[0] = new QuickSortThread();
+            sThreads[1] = new CombSortThread();
+            sThreads[2] = new SelectionSortThread();
+            sThreads[3] = new InsertionSortThread();
 
-        QuickSort.sort(list, ComparatorUtils.integerComparator());
+            Thread[] threads = new Thread[sThreads.length];
+            for (int i = 0; i < sThreads.length; i++)
+                threads[i] = new Thread(sThreads[i]);
+            for (Thread t : threads)
+                t.start();
+            for (Thread t : threads)
+                t.join();
 
-        System.out.println(Arrays.toString(list.toArray()));
+            System.out.println();
+            System.out.println();
+        }
+    }
+
+    private static abstract class SortThread implements Runnable
+    {
+        protected List<Integer> list;
+        protected Clock timer;
+
+        public SortThread()
+        {
+            list = new ArrayList<>(NUM_ELEMENTS);
+            randomList(list, NUM_ELEMENTS);
+            timer = Clock.systemDefaultZone();
+        }
+    }
+
+    private static class QuickSortThread extends SortThread
+    {
+        public QuickSortThread()
+        {
+            super();
+        }
+
+        public void run()
+        {
+            long start = timer.millis();
+            QuickSort.sort(list, ComparatorUtils.integerComparator());
+            long stop = timer.millis();
+            System.out.println("Quick sort done!");
+            double deltaTime = ((double) (stop - start)) / 1000.0;
+            System.out.println("Quick Sort time: " + deltaTime);
+        }
+    }
+
+    private static class CombSortThread extends SortThread
+    {
+
+        public CombSortThread()
+        {
+            super();
+        }
+
+        @Override
+        public void run()
+        {
+            long start = timer.millis();
+            CombSort.sort(list, ComparatorUtils.integerComparator());
+            long stop = timer.millis();
+            System.out.println("Comb sort done!");
+            double deltaTime = ((double) (stop - start)) / 1000.0;
+            System.out.println("Comb Sort time: " + deltaTime);
+        }
+    }
+
+    private static class InsertionSortThread extends SortThread
+    {
+
+        public InsertionSortThread()
+        {
+            super();
+        }
+
+        @Override
+        public void run()
+        {
+            long start = timer.millis();
+            InsertionSort.sort(list, ComparatorUtils.integerComparator());
+            long stop = timer.millis();
+            System.out.println("InsertionSort sort done!");
+            double deltaTime = ((double) (stop - start)) / 1000.0;
+            System.out.println("InsertionSort time: " + deltaTime);
+        }
+    }
+
+    private static class SelectionSortThread extends SortThread
+    {
+        public SelectionSortThread()
+        {
+            super();
+        }
+
+        @Override
+        public void run()
+        {
+            long start = timer.millis();
+            SelectionSort.sort(list, ComparatorUtils.integerComparator());
+            long stop = timer.millis();
+            System.out.println("Selection sort done!");
+            double deltaTime = ((double) (stop - start)) / 1000.0;
+            System.out.println("Selection Sort time: " + deltaTime);
+        }
     }
 }
